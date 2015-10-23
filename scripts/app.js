ab2f1e469efab5893ba5a910125db862872ab76d
Tavo <tavoasenjo@gmail.com>
2015-10-23 15:00:03 -0400

    last ugly stuff

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ artistList.artistEndPoint = 'http://developer.echonest.com/api/v4/artist/profile
  artistList.artistChoices = ["A-Trak", "A.C. Newman", "Arkells", "Astrocolor", "Bear Mountain", "Boys Noize", "Chromeo", "Crystal Castles", "Dark Horses", "Datsik", "DCF", "Death From Above 1979", "Dirty Ghosts", "Dubmatix", "Egyptrixx", "Electric Youth", "Emily Haines", "Fan Death", "Fur Trade", "Gazelle Twin", "Harrison", "Hiawatha", "Hot As Sun", "Huoratron", "Jack Novak", "JOOJ", "k-os", "Kay", "Kinnie Starr", "Leisure Cruise", "Let’s Go To War", "Lights", "Lindi Ortega", "Magneta Lane", "Metric", "Mixhell", "Modern Superstitions", "Moon King", "Mother Mother", "MSTRKRFT", "O’Luge", "Ohbijou", "Operators", "Panurge", "Purity Ring",	"Rush Midnight", "Ryan Hemsworth", "Sebastien Grainger",
 	"Seoul", "Sheldon Scott", "Silver Starling", "Terry Lynn", "The Burning Hell", "The New Pornographers", "Tiga", "Tre Mission", "We Are Enfant Terrible"]
 
-// console.log(artistList.artistChoices.length - 1);
+//
 
 artistList.artistRequest = function(artname){
 	$.ajax({
@@ -132,9 +132,6 @@ artistList.artistBlog = function(artistProfile){
 /* 
    Ends here
    ========================================================================== */
-
-
-
 
 // autocomplete
 artistList.init = function(){
